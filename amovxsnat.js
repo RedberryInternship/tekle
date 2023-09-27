44ada97c6c930015f26f52ee63fcab695c7019fa
Tekotatarishvili <tekletatarishvili@redberry.ge>
2023-09-27 13:34:42 +0400

    innitial commit

---
+++ amovxsnat.js
@@ -0,0 +1,21 @@
+let input = '';
+
+const solve = () => {
+    let [n, priceMoon, priceVenera] = input.split('\n')[0].split(' ').map(Number);
+
+    let lokacia=input.split('\n')
+    let countMoon=0
+    let countVenera=0
+
+    for(i=1;i<=n;i++){
+        if(lokacia[i].trim() =="Moon")
+            countMoon++
+        if(lokacia[i].trim() =="Venera")
+            countVenera++
+    }
+
+    console.log(priceMoon*countMoon + priceVenera*countVenera)
+}
+
+process.stdin.on('data', (c) => input += c);
+process.stdin.on('end', solve);
